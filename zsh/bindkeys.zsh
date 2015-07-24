d125982791a7a132dda60b2e781eb396361c0888
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-07-23 20:02:22 -0700

    updating .zsh

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -5,3 +5,16 @@
 noop () { }
 zle -N noop
 bindkey -M vicmd '\e' noop
+
+# vi history search fix
+autoload vi-search-fix
+zle -N vi-search-fix
+bindkey -M viins '\e/' vi-search-fix
+
+
+# other vi key fixes
+bindkey "^?" backward-delete-char
+bindkey "^W" backward-kill-word
+# Control-h also deletes the previous char
+bindkey "^H" backward-delete-char
+bindkey "^U" backward-kill-line 
