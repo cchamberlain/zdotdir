0a825b41d42727e79ee9e21b6fb74b6c8e79e959
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-09-19 17:02:40 -0700

    updates to exports

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -41,15 +41,15 @@ if [[ $IS_WIN -eq 1 ]]; then
   export PYTHON=/usr/bin/python
   export JAVA_HOME=/c/WINDOWS/system32/java
   export PF86=/c/Program\ Files\ \(x86\)
-  export PF=/c/Program\ Files
-  export CHROME_PATH=$PF86/Google/Chrome/Application/chrome.exe
-  export EDITOR_VS=$PF86/Microsoft\ Visual\ Studio\ 12.0/Common7/IDE/devenv.exe
-  export EDITOR_WEBSTORM=$PF86/JetBrains/WebStorm\ 10.0.4/bin/WebStorm64.exe
-  export PATH="/usr/local/bin:/usr/bin:/bin:/opt/bin:/mingw64/bin:$LOCAL_ROOT/bin:$LOCAL_ROOT/npm:$ZPYTHONDIR:$LOCAL_ROOT/twisted/bin:$LOCAL_ROOT/iojs:$LOCAL_ROOT/tcc:/c/WINDOWS/system32:/c/WINDOWS:$PF86/Windows\ Kits/8.0/bin/x64:/c/WINDOWS/System32/WindowsPowerShell/v1.0:$PF86/Heroku/bin:$LOCAL_ROOT/chocolatey/bin:$HOME/AppData/Local/atom/bin"
+  export PF="/c/Program Files"
+  export CHROME_PATH="$PF86/Google/Chrome/Application/chrome.exe"
+  export EDITOR_VS="$PF86/Microsoft Visual Studio 12.0/Common7/IDE/devenv.exe"
+  export EDITOR_WEBSTORM="$PF86/JetBrains/WebStorm 10.0.4/bin/WebStorm64.exe"
+  export PATH="/usr/local/bin:/usr/bin:/bin:/opt/bin:/mingw64/bin:$LOCAL_ROOT/bin:$PF86/Microsoft VS Code/bin:$LOCAL_ROOT/npm:$ZPYTHONDIR:$LOCAL_ROOT/twisted/bin:/c/HashiCorp/Vagrant/bin:$PF/Docker Toolbox:$PF/Oracle/VirtualBox:$PF86/VMware/VMware Workstation/x64:$PF86/VMware/VMware Workstation:$LOCAL_ROOT/iojs:$PF/Java/jre1.8.0_60/bin:$LOCAL_ROOT/tcc:/c/WINDOWS/system32:/c/WINDOWS:$PF86/Windows Kits/8.0/bin/x64:/c/WINDOWS/System32/WindowsPowerShell/v1.0:$PF86/Heroku/bin:$LOCAL_ROOT/chocolatey/bin:$HOME/AppData/Local/atom/bin"
 elif [[ $IS_MAC -eq 1 ]]; then
   export TERM=xterm-256color
   export CHROME_PATH="$(which chrome)"
-  export PATH="$USR_BIN_ROOT:$NPM_CONFIG_PREFIX/bin:$PATH:~/bin:/usr/local/bin:/usr/local/sbin:~/bin"
+  export PATH="$USR_BIN_ROOT:$NPM_CONFIG_PREFIX/bin:$PATH:$HOME/bin:/usr/local/bin:/usr/local/sbin:$HOME/bin"
   export EDITOR_WEBSTORM="wstorm"
 fi
 
